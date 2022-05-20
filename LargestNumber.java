dacb0e72a415f092856a318cc9b3ecfd8e08be27
logaraajan <105904883+logaraajan@users.noreply.github.com>
2022-05-20 11:23:05 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,25 @@
+package com.edu.ind;
+import java.util.Scanner;
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,num3;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the 1st number");
+		num1=sc.nextInt();
+		System.out.println("Enter the 2nd number");
+		num2=sc.nextInt();
+		System.out.println("Enter the 3rd number");
+		num3=sc.nextInt();
+		if(num1>num2 && num1>num3) {
+			System.out.println(+num1+" is greater than "+num2+" and "+num3);
+		}else if (num2>num3) {
+			System.out.println(+num2+" is greater than "+num1+" and "+num3);
+		}else { 
+			System.out.println(+num3+" is greater than "+num1+" and "+num2);
+		}
+
+	}
+
+}
