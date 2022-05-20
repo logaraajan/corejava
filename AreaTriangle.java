8910b72f64bad69413f2be7c858a9d4994ea61e4
logaraajan <105904883+logaraajan@users.noreply.github.com>
2022-05-20 11:21:34 +0530

    Add files via upload

---
+++ AreaTriangle.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class AreaTriangle {
+
+	public static void main(String[] args) {
+		int height,base,area;
+		height=52;
+		base=12;
+		area=base*height/2;
+		
+		System.out.println("the area of triangle has height="+height+" and base="+base+" it's area is " +area +"squnits");
+  
+	}
+
+}
