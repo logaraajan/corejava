14b0b171794e89d5c0192d67dcf4e3b22ea61707
logaraajan <105904883+logaraajan@users.noreply.github.com>
2022-05-20 11:21:15 +0530

    Add files via upload

---
+++ AreaSquare.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class AreaSquare {
+
+	public static void main(String[] args) {
+		int side,area;
+		side=68;
+		
+		area=side*side;
+		
+		System.out.println("the area of square has a side="+side+" and the area is "+area +"squnits");
+
+	}
+
+}
