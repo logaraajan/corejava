52d9cef90d3f1f23bc380cce29a74d5038a5bb15
logaraajan <105904883+logaraajan@users.noreply.github.com>
2022-05-20 12:16:42 +0530

    Add files via upload

---
+++ EvenNatural.java
@@ -0,0 +1,20 @@
+package project5;
+
+public class EvenNatural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,sum;
+		sum=0;
+		i=2;
+		while(i<=100) {
+			sum=sum+i; 
+			i=i+2;
+		}
+	    System.out.println("sum=" +sum );
+	    
+	    
+	}
+        
+	
+}
