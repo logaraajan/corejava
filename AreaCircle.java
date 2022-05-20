5af4fc70c7f256713e2fbcef12511c95af577da4
logaraajan <105904883+logaraajan@users.noreply.github.com>
2022-05-20 11:20:26 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class AreaCircle {
+
+	public static void main(String[] args) {
+		float radius,areaf;
+		radius=60;
+		areaf=3.14159f*radius*radius;
+		
+		System.out.println ( "the area of circle has radius= "+radius+" then it's area is "+areaf +"squnits" );
+		
+
+	}
+
+}
