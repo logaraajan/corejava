8e5ba835ad1cf4ff60a5a5a161d0fb0f4f356f84
logaraajan <105904883+logaraajan@users.noreply.github.com>
2022-05-20 11:20:51 +0530

    Add files via upload

---
+++ AreaReactangle.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class AreaReactangle {
+
+	public static void main(String[] args) {
+		int length,breadth,area;  //declaration
+		length =25;   
+		breadth =15;
+		area=length*breadth;
+		
+		System.out.println("the area of reactangle length="+length+" and breadth="+breadth+" and the area is " +area +"squnits");
+		
+
+	}
+
+}
